3de27c4b145b66682edc663502fc8bae84fa38e7
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-21 19:13:27 -0700

    Create  generate_test_images.py

---
+++ Identify Vehicle Models with WhoIsThisTool/generate_test_images.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import cv2
+import numpy as np
+
+def create_vehicle_image(filename, color, vehicle_type):
+    """
+    Create a test vehicle image with specified characteristics.
+    
+    TODO: Implement image creation with:
+    - Background color based on vehicle color
+    - Text overlay with vehicle type
+    - Simulated license plate region
+    - Vehicle dimensions appropriate to type
+    """
+    # TODO: Create blank image (800x600)
+    # TODO: Add colored background
+    # TODO: Draw vehicle outline/shape
+    # TODO: Add text labels
+    # TODO: Save image
+    pass
+
+# TODO: Create test images for sedan, SUV, and truck
+# create_vehicle_image("sedan.jpg", (100, 100, 200), "SEDAN")
+# create_vehicle_image("suv.jpg", (200, 100, 100), "SUV")
+# create_vehicle_image("truck.jpg", (100, 200, 100), "TRUCK")
